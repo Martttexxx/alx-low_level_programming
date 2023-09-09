e1dbf5bda18192ff57399b02fcf297c12b722534
Martttexxx <martinnwosu601@gmail.com>
2023-09-08 19:37:28 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+  *main - this is the main function
+  *Return: 0 (success)
+  */
+
+int main(void)
+
+{
+char b;
+
+for (b = 'z'; b >= 'a'; b--)
+putchar(b);
+
+putchar('\n');
+return (0);
+
+
+
+
+
+
+
+
+
+}
