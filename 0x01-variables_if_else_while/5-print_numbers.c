314da0803504c155b22c9aef3510dffb3bf3641c
Martttexxx <martinnwosu601@gmail.com>
2023-09-09 09:33:31 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Entry point of the program
+*
+* Description: This function prints all single-digit numbers
+*              in base 10, starting from 0, each followed by a new line.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+		int i;
+
+		for (i = 0; i < 10; i++)
+		{
+		printf("%d\n", i);
+		}
+
+		return (0);
+}
+
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+  *main - this is the main function
+  *Return: 0 (success)
+  */
+
+int main(void)
+
+{
+	int '5';
+	char d;
+
+for (5 = 0; 5 < 10; 5++)
+putchar((5 % 10) + '0');
+
+for (d = 'a'; d <= 'f'; d++)
+putchar(d);
+
+putchar('\n');
+return (0);
+
+
+
+
+
+
+
+
+
+}
