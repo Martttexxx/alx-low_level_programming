bb9808da14aadbc343191bde48a8de64eb14ca07
Martttexxx <martinnwosu601@gmail.com>
2023-10-02 08:04:49 -0700

    libmy.a

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include<stdio.h>
+/**
+*_puts - a function that prints a string, followed by a new line, to stdout
+*@str: string to p[rint
+* Return: void
+*/
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+		_putchar('\n');
+}
