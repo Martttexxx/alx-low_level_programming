d27c293763b39d8a526a86e45de425abd711bce3
Martttexxx <martinnwosu601@gmail.com>
2023-10-22 23:54:41 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+void LetsGo(void)__attribute__ ((constructor));
+/**
+*LetsGo - this function runs before main
+*/
+void LetsGo(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+
+
+}
+
